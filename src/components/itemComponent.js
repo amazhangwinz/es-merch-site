19a006d81386c2a35352e3941c74419172983143
Charlotte Lai <charlotte.lai@unswengsoc.com>
2023-06-19 09:38:16 +0000

    Update itemComponent.js, not sure if this is correct

--- src/components/itemComponent.js
+++ src/components/itemComponent.js
@@ -3,11 +3,30 @@ import React from 'react';
 const ItemDescription = ({ productName, price, description }) => {
   return (
     <div>
-      <h2>{productName}</h2>
-      <h3>Price: ${price}</h3>
+      <h1>{productName}</h1>
+      <h2>Price: ${price}</h2>
       <p>{description}</p>
     </div>
   );
 };
 
 export default ItemDescription;
+
+const SingleItemPage = () => {
+  const productName = "Limited Edition Plush";
+  const price = "$24.50";
+  const description = "If you love baking cookies, eating almond pound cake and cuddling with cute animals, you will adore this sanrio my melody plush.";
+
+  return (
+    <div>
+      <h1>Single Item Page</h1>
+      <ItemDescription
+        productName={productName}
+        price={price}
+        description={description}
+      />
+    </div>
+  );
+};
+
+export default SingleItemPage;
