19a4ee80b86ec9344736acedc20402b16943c893
Emily D <emilydang023@gmail.com>
2023-06-21 22:59:08 +1000

    Created CartCard component

---
+++ src/components/CartCard.tsx
@@ -0,0 +1,107 @@
+import React from 'react';
+import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
+
+interface Prop {
+	image: string;
+	title: string;
+	description: string;
+	colour: string;
+	size: string;
+	unitPrice: number;
+	quantity: string;
+	removeItem: string;
+	total: number;
+}
+
+const StyledProduct = styled.div`
+	display: flex;
+	height: 20vw;
+	width: 90vw;
+	background: #fbf9f9;
+	margin: 1.75vw;
+	border: 1px solid black
+`;
+
+const StyledProperty = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	// border: 1px solid black
+`;
+
+const StyledImage = styled.img`
+	width: 100%;
+`;
+
+// const StyledImage = styled.img`
+// 	width: ${(props) => props.width || "100%"};
+// `;
+
+const StyledText = styled.div`
+	padding: 1rem;
+	box-sizing: border-box;
+	text-align: left;
+`;
+
+const StyledTitle = styled.div`
+	font-size: 25px
+`;
+const StyledInfo = styled.div`
+	font-size: 15px
+`;
+
+const StyledTotal = styled.div`
+	flex: 1;
+	display: flex;
+	flex-direction: column;
+	justify-content: flex-start;
+	padding: 1rem;
+	box-sizing: border-box;
+	align-items: flex-start;
+`;
+
+const CartCard = (props: Prop) => {
+	return (
+		
+		<StyledProduct>
+			<StyledProperty style = {{width: '20%'}}>
+  				<StyledImage style = {{width: '100%'}} src={props.image} alt=""/>
+			</StyledProperty>
+			<StyledProperty style = {{width: '30%'}}> 
+				<StyledText>
+					<StyledTitle>{props.title}</StyledTitle>
+					<StyledInfo>Product: {props.description}</StyledInfo>
+					<StyledInfo>Colour: {props.colour}</StyledInfo>
+					<StyledInfo>Size: {props.size}</StyledInfo>
+					<StyledInfo>Unit Price: ${props.unitPrice}</StyledInfo>
+				</StyledText>
+			</StyledProperty>
+			<StyledProperty style = {{width: '20%'}}> 
+				<StyledImage style = {{width: '65%'}} src={props.quantity} alt=""/>
+			</StyledProperty>
+			<StyledProperty style = {{width: '10%'}}> 
+				<StyledImage style = {{width: '30%'}}src={props.removeItem} alt=""/>
+			</StyledProperty>
+			<StyledProperty style = {{width: '20%'}}>  
+				<StyledTitle>${props.total}</StyledTitle>
+			</StyledProperty>
+
+
+			{/* <StyledText>
+				<StyledTitle>{props.title}</StyledTitle>
+				<StyledInfo>Product: {props.description}</StyledInfo>
+				<StyledInfo>Colour: {props.colour}</StyledInfo>
+				<StyledInfo>Size: {props.size}</StyledInfo>
+				<StyledInfo>Unit Price: ${props.unitPrice}</StyledInfo>
+			</StyledText>
+			<StyledImage src={props.quantity} alt="" />
+			<StyledImage src={props.removeItem} alt="" />
+			<StyledText>
+				<StyledTitle>${props.total}</StyledTitle>
+			</StyledText> */}
+		</StyledProduct>
+	);
+};
+
+export default CartCard;
--- src/pages/Cartpage.tsx
+++ src/pages/Cartpage.tsx
@@ -1,11 +1,29 @@
 import React from 'react'
+import CartCard from '../components/CartCard';
+import Winnie from '../assets/WinnieZhang.png'
+import Quantity from '../assets/Quantity.png'
+import Bin from '../assets/Bin.png'
+
 
 const Cartpage = () => {
   return (
     <div>
       <h1>
-        CART PAGE
+        Your Cart
       </h1>
+      <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center', alignItems: 'center' }}>
+				<CartCard
+          image={Winnie} 
+          title="Winnie Zhang"
+          description="Limited Edition Hoodie"
+          colour="Black"
+          size="Small"
+          unitPrice={75}
+          quantity={Quantity} 
+          removeItem={Bin} 
+          total={200} 
+          />
+			</div>
     </div>
   )
 }
