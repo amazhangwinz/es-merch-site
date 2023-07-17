b701cfbea4aaa3cc95b4786b265ac4891028a5d7
Neel Dutta <neeldutta@Neels-MacBook-Air.local>
2023-07-17 20:04:15 +1000

    Example of using useContext to display items in a cart

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,56 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Homepage from './pages/Homepage';
 import Cartpage from './pages/Cartpage';
+import ExampleUseContext from './components/ExampleUseContext';
+
+/* 
+- quantity of the items
+- items in cart
+*/
+
+export interface CartItem {
+  price: number,
+  name: string,
+  quantity: number,
+}
+
+const defaultCart: CartItem[] = [
+  {
+    price: 100,
+    name: "jacket",
+    quantity: 10
+  },
+  {
+    price: 100,
+    name: "hat",
+    quantity: 2
+  },
+  {
+    price: 20,
+    name: "scarf",
+    quantity: 10
+  },
+];
+
+export const cartContext = React.createContext({
+  cart: defaultCart,
+  setCart: (newCart: CartItem[]) => {}, 
+})
 
 function App() {
+  const [cart, setCart] = React.useState<CartItem[]>(defaultCart);
   return (
-    <div className="App">
+    <cartContext.Provider value={{ cart, setCart }}>
       <BrowserRouter>
+        <ExampleUseContext></ExampleUseContext>
         <Navbar />
+        <></>
         <Routes>
           <Route path='/' element={<Homepage />} />
           <Route path='/cart' element={<Cartpage />} />
         </Routes>
       </BrowserRouter>
-    </div>
+    </cartContext.Provider>
   );
 }
 
