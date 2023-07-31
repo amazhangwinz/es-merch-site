04fef653541fa63659262c99f3a861dc6a2643ed
S0l4RIS9uk <115390498+S0l4RIS9uk@users.noreply.github.com>
2023-07-31 19:47:51 +1000

    Better Styling

--- src/components/ItemCard.tsx
+++ src/components/ItemCard.tsx
@@ -13,7 +13,7 @@ interface Prop {
 const StyledComponent = styled.div`
 	display: flex;
 	height: 10vw;
-	width: 30vw;
+	width: 25vw;
 	border-radius: 10px;
 	background: #fbf9f9;
 	margin: 1.75vw;
@@ -21,13 +21,16 @@ const StyledComponent = styled.div`
 `;
 
 const StyledContent = styled.div`
+	height: 100%;
+	width: 65%;
 	flex: 1;
 	display: flex;
 	flex-direction: column;
-	justify-content: flex-start;
-	padding: 1rem;
+	justify-content: space-evenly;
+	padding: 2rem;
 	box-sizing: border-box;
 	align-items: flex-start;
+	position: relative;
 `;
 
 const StyledImage = styled.img`
@@ -35,34 +38,36 @@ const StyledImage = styled.img`
 	width: auto;
 	max-width: 33%;
 	border-radius: 0 10px 10px 0;
+	object-fit: cover;
 `;
 
 const StyledPrice = styled.p`
 	margin-bottom: 0;
+	font-size: 95%
 `;
 
 const StyledDescription = styled.p`
-	max-width: 60vw;
-	max-height: 10vw;
+	max-width: 100%;
+	max-height: 70%;
 	overflow: hidden;
 	text-overflow: ellipsis;
 	overflow: hidden;
 	word-wrap: break-word;
+	font-size: 95%;
 `;
 
 const StyledColours = styled.p`
-	display: flex;
+	display: inline-flex;
 	flex-direction: row;
 	justify-content: flex-start;
 	margin: 0;
+	padding-right: 5%
 `;
 
 const ColourCircles = styled.div`
 	height: 20px;
 	width: 20px;
 	border-radius: 50%;
-	margin: 0;
-	margin-right: 0.5rem;
 	box-shadow: 1px 1.5px 0.5px silver;
 `;
 
@@ -75,16 +80,18 @@ const ItemCard = (props: Prop) => {
 	return (
 		<StyledComponent>
 			<StyledContent>
-				<h1 onClick={productPage} style={{ margin: 0, cursor: 'pointer', fontSize: '130%' }}>
+				<h1 onClick={productPage} style={{ margin: 0, cursor: 'pointer', fontSize: '125%' }}>
 					{props.title}
 				</h1>
-				<StyledDescription style={{ margin: 0, textAlign: 'left', height: '3rem' }}>{props.description}</StyledDescription>
-				<StyledPrice style={{ margin: 0, marginBottom: 5 }}>${props.price}</StyledPrice>
-				<StyledColours style={{ margin: 0 }}>
+				<StyledDescription style={{ margin: 0, textAlign: 'left'}}>{props.description}</StyledDescription>
+				<StyledColours style={{ margin: 0, marginBottom: '2.5%'}}>
 					{props.colours.map((colour) => (
-						<ColourCircles style={{ background: `linear-gradient(190deg, ${colour}, grey 125%)` }}></ColourCircles>
-					))}
+						<div style={{paddingRight: '5%', width: '100%', height: '100%'}}>
+							<ColourCircles style={{ background: `linear-gradient(190deg, ${colour}, grey 125%)` }}></ColourCircles>
+						</div>
+						))}
 				</StyledColours>
+				<StyledPrice style={{ margin: 0 }}>${props.price}</StyledPrice>
 			</StyledContent>
 			<StyledImage src={props.image} alt="" />
 		</StyledComponent>
