68f4ab3ab470b9f808832a64d5f97e2dd241e43f
Girichand <g.jupudi@student.unsw.edu.au>
2023-09-08 20:10:20 +1000

    spacing

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -1,5 +1,4 @@
-import React from 'react';
-
+import React, { useEffect, useState } from 'react';
 import heroImage from '../assets/wholeEngSoc.webp';
 import { Hero } from '../components/Hero';
 import ItemCard from '../components/ItemCard';
@@ -7,9 +6,16 @@ import { data } from '../Data.jsx';
 import { Box } from '@mui/material';
 
 const Homepage = () => {
+
 	return (
 		<div>
-			<Hero title="EngSoc 2023" tagLine="Braincells not included." image={heroImage} itemName="test"></Hero>
+			<Hero
+				title="EngSoc 2023"
+				tagLine="Braincells not included."
+				image={heroImage}
+				itemName="test"
+			/>
+
 			<Box
 				sx={{ flexFlow: { md: 'row wrap', xs: 'column' } }}
 				style={{
@@ -19,6 +25,7 @@ const Homepage = () => {
 					alignItems: 'center',
 					marginLeft: '10vw',
 					marginRight: '10vw',
+					zIndex: '1',
 				}}
 			>
 				{data.map((item, idx) => {
