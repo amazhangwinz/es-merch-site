c26d3ba0fce005d97e452b10c8b7f2663e591c50
Girichand <g.jupudi@student.unsw.edu.au>
2023-09-12 10:28:08 +1000

    changed item images

--- src/Data.jsx
+++ src/Data.jsx
@@ -1,36 +1,44 @@
-// import plush from "./assets/plush.jpg";
-// import pushin from "./assets/pushin.gif";
-// import winz from "./assets/WinnieZhang.webp";
-// import item from "./assets/728a14b7c377e3a51bf325b237c74de8.jpg";
-// import item1 from "./assets/lighter.webp"
 import HoodieSizeGuide from "./assets/HoodieSizeGuide.webp"
 import JumperSizeGuide from "./assets/JumperSizeGuide.webp"
-import OG from "./assets/OG.webp"
-import Sketch from "./assets/SketchBooked.webp"
-import PN from "./assets/PastelNights.webp"
+import MorningGreen from "./assets/greenhoodie.png"
+import MidnightBlue from "./assets/bluehoodie.webp"
+import SilveryMoon from "./assets/greyhoodie.webp"
+import PastelNights from "./assets/PastelNights.webp"
 
 export const data = [{
-    "name": "The Original",
+    "name": "Morning Green",
     "description": "Imagine wrapping yourself in a soft lush green meadow. Our EngSoc Crewneck isn't just clothing; it's a statement!",
+    "details": "Entire design embroidered",
     // "colours": [{ name: "Green", img: OG, primaryColour: "#FB95BA", secondaryColour: "#FEC8DC" }, { name: "Purple", img: pushin, primaryColour: "#EBB1FF", secondaryColour: "#F5D8FF" }, { name: "Orange", img: winz, primaryColour: "#FF7A00", secondaryColour: "#FED5B0" }],
     "price": 55.00,
     "sizeguide": JumperSizeGuide,
-    default: OG
+    default: MorningGreen
 },
 {
-    "name": "Sketchbooked",
-    "description": "Whether you're conquering campus, chilling at home, or embarking on epic adventures, this hoodie effortlessly blends comfort and style.",
+    "name": "Midnight Blue",
+    "description": "Elevate your engineering style with EngSoc's Navy Hoodie, featuring a sleek navy design, superior comfort, and a perfect fit for versatile, cozy wear that showcases your engineering spirit.",
+    "details": "Entire design embroidered",
     // "colours": [{ name: "Pink", img: Sketch, primaryColour: "#FB95BA", secondaryColour: "#FEC8DC" }, { name: "Purple", img: pushin, primaryColour: "#EBB1FF", secondaryColour: "#F5D8FF" }, { name: "Orange", img: winz, primaryColour: "#FF7A00", secondaryColour: "#FED5B0" }],
     "price": 55.00,
     "sizeguide": HoodieSizeGuide,
-    default: Sketch
+    default: MidnightBlue
+},
+{
+    "name": "Silvery Moon",
+    "description": "Upgrade your style with EngSoc's Grey Hoodie, featuring a sleek design and ultimate comfort.",
+    "details": "Entire design embroidered",
+    // "colours": [{ name: "Pink", img: Sketch, primaryColour: "#FB95BA", secondaryColour: "#FEC8DC" }, { name: "Purple", img: pushin, primaryColour: "#EBB1FF", secondaryColour: "#F5D8FF" }, { name: "Orange", img: winz, primaryColour: "#FF7A00", secondaryColour: "#FED5B0" }],
+    "price": 55.00,
+    "sizeguide": HoodieSizeGuide,
+    default: SilveryMoon
 },
 {
     "name": "Pastel Nights",
     "description": "From chilly winter nights to breezy summer evenings, this hoodie keeps you cozy all year long.",
+    "details": "front logo embroidered, back design screen printed",
     // "colours": [{ name: "Pink", img: PN, primaryColour: "#FB95BA", secondaryColour: "#FEC8DC" }, { name: "Purple", img: pushin, primaryColour: "#EBB1FF", secondaryColour: "#F5D8FF" }, { name: "Orange", img: winz, primaryColour: "#FF7A00", secondaryColour: "#FED5B0" }],
     "price": 55.00,
     "sizeguide": HoodieSizeGuide,
-    default: PN
+    default: PastelNights
 },
 ]
